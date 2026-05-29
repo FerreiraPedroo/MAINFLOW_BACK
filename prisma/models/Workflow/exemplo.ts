22fe6ee235686a48e225caf633cbfe3e3840b56a
FerreiraPedroo <pedroohenrique@hotmail.com>
2026-05-29 16:57:48 -0300

    Update

---
+++ prisma/models/Workflow/exemplo.ts
@@ -0,0 +1,14 @@
+const processModel = {
+  id: 1,
+  title: 'Criar projeto',
+};
+
+const processSteps = {
+    
+}
+
+const processInstance = {
+  id: 1,
+  process_id: 1,
+  steps: [{ id: 1, sequence: 1, parallel: true, process_instance_id: 1 }],
+};
